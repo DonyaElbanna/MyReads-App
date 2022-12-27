8dade2c937ce99bbeb3c77d1b49d2c10a04f93fa
Donya Elbanna <donyaelbanna@yahoo.com>
2022-12-27 22:58:55 +0200

    Updates

---
+++ precache-manifest.ad11d0c8187d92d2134bb3b1442a3fd5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ef7087a586ef395bd31",
+    "url": "/MyReads-App/static/css/main.161731db.chunk.css"
+  },
+  {
+    "revision": "8ef7087a586ef395bd31",
+    "url": "/MyReads-App/static/js/main.8ef7087a.chunk.js"
+  },
+  {
+    "revision": "8b6ac2f8e7822004e4bc",
+    "url": "/MyReads-App/static/js/1.8b6ac2f8.chunk.js"
+  },
+  {
+    "revision": "b0806c66941ffaab482d",
+    "url": "/MyReads-App/static/js/runtime~main.b0806c66.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/MyReads-App/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/MyReads-App/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/MyReads-App/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "b41e35b66abaebca46486d93e0a9c3e7",
+    "url": "/MyReads-App/index.html"
+  }
+];
